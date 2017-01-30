84074667bad77b92b6f68ee23a723008c130defa
Daniel Fineberg <Daniel@Daniels-MBP.home>
2017-01-30 23:17:17 +0000

    Begun building AbstractTween system

---
+++ Assets/Scripts/GameTemplate/Animation/Tweening/AbstractTween.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Promises;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public abstract class AbstractTween<T> : MonoBehaviour {
+
+	[RangeAttribute(0f, 1f)]
+	public float testPosition;
+
+	public T fromValue;
+	public T toValue;
+
+	public float duration;
+	public float delay;
+
+	public Easing.Functions easeType;
+
+
+	public abstract IPromise<AbstractTween<T>> Animate();
+
+	public abstract T GetValueAtPoint(float normalisedPoint);
+
+	protected abstract void SetValue(float normalisedPoint);
+
+	protected virtual void Update()
+	{
+		if(Application.isEditor && !Application.isPlaying)
+			SetValue(testPosition);
+	}
+}
---
+++ Assets/Scripts/GameTemplate/Animation/Tweening/PositionTween.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Promises;
+using UnityEngine;
+
+[AddComponentMenu("Animation/Position Tween")]
+public class PositionTween : AbstractTween<Vector3>
+{
+	public bool animateInWorldPosition;
+
+	void Start()
+	{
+		if(Application.isPlaying)
+			Animate();
+	}
+
+    public override IPromise<AbstractTween<Vector3>> Animate()
+    {
+		var p = new Promise<AbstractTween<Vector3>>();
+
+		CoroutineExtensions.WaitForSeconds(delay)
+		.ThenTween(
+			duration,
+			f => SetValue(f)
+		)
+		.ThenDo(o => p.Resolve(this));
+
+		return p;
+    }
+
+    public override Vector3 GetValueAtPoint(float normalisedPoint)
+    {
+        return Vector3.Lerp(fromValue, toValue, Easing.Interpolate(normalisedPoint, easeType));
+    }
+
+    protected override void SetValue(float normalisedPoint)
+    {
+        if(animateInWorldPosition)
+			transform.position = GetValueAtPoint(normalisedPoint);
+		else
+			transform.localPosition = GetValueAtPoint(normalisedPoint);
+    }
+}
