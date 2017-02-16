15b9004ac4e2038383178e4ea29f2c9dfbc9e7bb
Daniel Fineberg <daniel@sfbgames.com>
2017-02-16 16:51:21 +0000

    Added SceneManagerExtensions for loading/unloading scenes asynchronously using Promises

---
+++ Assets/Scripts/GameTemplate/Extensions/SceneManagerExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Promises;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneManagerExtensions
+{
+    public static IPromise LoadSceneAsync(string sceneName, LoadSceneMode mode = LoadSceneMode.Additive)
+    {
+        Scene scene = default(Scene);
+
+        return CoroutineExtensions.WaitUntil(SceneManager.LoadSceneAsync(sceneName, mode))
+            .ThenDo(() => scene = SceneManager.GetSceneByName(sceneName))
+            .ThenWaitUntil(() => scene.isLoaded);
+    }
+
+    public static IPromise LoadSceneAsync(int sceneBuildIndex, LoadSceneMode mode = LoadSceneMode.Additive)
+    {
+        Scene scene = default(Scene);
+
+        return CoroutineExtensions.WaitUntil(SceneManager.LoadSceneAsync(sceneBuildIndex, mode))
+            .ThenDo(() => scene = SceneManager.GetSceneByBuildIndex(sceneBuildIndex))
+            .ThenWaitUntil(() => scene.isLoaded);
+    }
+
+    public static IPromise UnloadSceneAsync(int sceneBuildIndex)
+    {
+        return CoroutineExtensions.WaitUntil(SceneManager.UnloadSceneAsync(sceneBuildIndex));
+    }
+
+    public static IPromise UnloadSceneAsync(string sceneName)
+    {
+        return CoroutineExtensions.WaitUntil(SceneManager.UnloadSceneAsync(sceneName));
+    }
+
+    public static IPromise UnloadSceneAsync(Scene scene)
+    {
+        return CoroutineExtensions.WaitUntil(SceneManager.UnloadSceneAsync(scene));
+    }
+}
