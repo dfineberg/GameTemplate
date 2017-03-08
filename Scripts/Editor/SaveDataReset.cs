31e668070ba60acc75ba26f12d06d73770a429f2
Daniel Fineberg <Daniel@daniels-mbp.home>
2017-03-08 09:49:41 +0000

    Added editor script for deleting all files from persistent data path

---
+++ Scripts/Editor/SaveDataReset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class SaveDataReset {
+
+    [MenuItem("Tools/Reset Save Files")]
+    private static void ResetSaveFile()
+    {
+        foreach (var file in Directory.GetFiles(Application.persistentDataPath))
+            File.Delete(file);
+
+        foreach(var directory in Directory.GetDirectories(Application.persistentDataPath))
+            Directory.Delete(directory, true);
+    }
+}
