76761aaad8cbc3240ebf6f0580a6dab63232478e
Daniel Fineberg <Daniel@daniels-mbp.home>
2017-03-20 17:45:02 +0000

    Physics utility scripts

---
+++ Scripts/Physics/DestroyOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyOnEnter : MonoBehaviour
+{
+    public LayerMask DestroyLayers;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!DestroyLayers.ContainsLayer(other.gameObject.layer))
+            return;
+
+        Destroy(other.gameObject);
+    }
+}
