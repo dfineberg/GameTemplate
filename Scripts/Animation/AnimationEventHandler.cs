709766d3b9d636c23a5695b154dfc7c8ecdf992e
Daniel Fineberg <Daniel@daniels-mbp.home>
2017-05-12 18:08:25 +0100

    AnimationEventHandler

---
+++ Scripts/Animation/AnimationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimationEventHandler : MonoBehaviour
+{
+    public event Action<string> AnimationEvent;
+
+    public void FireEvent(string id)
+    {
+        if (AnimationEvent != null)
+            AnimationEvent(id);
+    }
+}
