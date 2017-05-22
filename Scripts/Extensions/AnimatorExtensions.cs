80cb00889249125c11092cbc9aef304c81b81a05
Daniel Fineberg <Daniel@Daniels-MacBook-Pro.local>
2017-05-22 17:56:03 +0100

    Added AnimatorExtensions

---
+++ Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Promises;
+using UnityEngine;
+
+public static class AnimatorExtensions
+{
+    public static IPromise WaitUntilState(this Animator animator, string state)
+    {
+        return CoroutineExtensions.WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName(state));
+    }
+}
