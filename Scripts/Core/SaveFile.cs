cdcc255e1fccd6a12353703b696b5d5bc28b7668
Daniel Fineberg <Daniel@Daniels-MacBook-Pro.local>
2017-05-25 16:51:03 +0100

    Updated save file for planet progress

--- Scripts/Core/SaveFile.cs
+++ Scripts/Core/SaveFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 [Serializable]
 public struct SaveFile
@@ -11,21 +12,43 @@ public struct SaveFile
     // planets progress
     public int[] PlanetProgress;
 
+    private void CheckArray()
+    {
+        if (PlanetProgress == null)
+            PlanetProgress = new int[GameManager.PlanetDefinitions.Length];
+        else if(PlanetProgress.Length < GameManager.PlanetDefinitions.Length)
+            Array.Resize(ref PlanetProgress, GameManager.PlanetDefinitions.Length);
+    }
+
+    public void PlanetVisited(int planetNo)
+    {
+        CheckArray();
+
+        PlanetProgress[planetNo]++;
+    }
+
     public float GetPlanetProgress(int planetNo)
     {
-        return (float) PlanetProgress[planetNo] / GameManager.PlanetDefinitions[planetNo].PlaysToComplete;
+        CheckArray();
+        
+        var f = (float) PlanetProgress[planetNo] / GameManager.PlanetDefinitions[planetNo].PlaysToComplete;
+        return Mathf.Clamp01(f);
     }
 
     public bool IsPlanetUnlocked(int planetNo)
     {
+        CheckArray();
+        
         if (planetNo == 0)
             return true;
 
-        return GetPlanetProgress(planetNo - 1) > 0.5f;
+        return GetPlanetProgress(planetNo - 1) >= 0.5f;
     }
 
     public bool[] GetPlanetUnlocks()
     {
+        CheckArray();
+        
         var unlocks = new bool[GameManager.PlanetDefinitions.Length];
 
         for (var i = 0; i < unlocks.Length; i++)
