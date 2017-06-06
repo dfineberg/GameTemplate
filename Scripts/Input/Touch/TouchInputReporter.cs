9af945021cfbad9f419674c5373e844838dada0a
Daniel Fineberg <Daniel@Daniels-MacBook-Pro.local>
2017-06-06 14:20:03 +0100

    TouchInput now works with 3D raycasts and 2D raycasts, added UnityEvents to TouchInputReporter

--- Scripts/Input/Touch/TouchInputReporter.cs
+++ Scripts/Input/Touch/TouchInputReporter.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class UnityTouchEvent : UnityEvent<Vector2>{}
+
+[Serializable]
+public class UnityTouchUpdateEvent : UnityEvent<Vector2, Vector2>{}
 
 public class TouchInputReporter : MonoBehaviour, ITouchInputHandler
 {
@@ -11,27 +19,40 @@ public class TouchInputReporter : MonoBehaviour, ITouchInputHandler
     public event TouchEventHandler OnTouchUp;
     public event TouchEventHandler OnTouchTap;
 
+    public UnityTouchEvent OnTouchDownUnityEvent;
+    public UnityTouchUpdateEvent OnTouchUpdateUnityEvent;
+    public UnityTouchEvent OnTouchUpUnityEvent;
+    public UnityTouchEvent OnTouchTapUnityEvent;
+
     public void HandleTouchDown(Vector2 touchPosition)
     {
         if (OnTouchDown != null)
             OnTouchDown(touchPosition);
+        
+        OnTouchDownUnityEvent.Invoke(touchPosition);
     }
 
     public void HandleTouchUpdate(Vector2 touchPosition, Vector2 touchPositionDelta)
     {
         if (OnTouchUpdate != null)
             OnTouchUpdate(touchPosition, touchPositionDelta);
+        
+        OnTouchUpdateUnityEvent.Invoke(touchPosition, touchPositionDelta);
     }
 
     public void HandleTouchUp(Vector2 touchPosition)
     {
         if (OnTouchUp != null)
             OnTouchUp(touchPosition);
+        
+        OnTouchUpUnityEvent.Invoke(touchPosition);
     }
 
     public void HandleTouchTap(Vector2 touchPosition)
     {
         if (OnTouchTap != null)
             OnTouchTap(touchPosition);
+        
+        OnTouchTapUnityEvent.Invoke(touchPosition);
     }
 }
