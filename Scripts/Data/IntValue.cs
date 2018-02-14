1087465c6c368829024d0e01980acc19769bb13d
Daniel <daniel@shed-works.co.uk>
2018-02-14 16:12:48 +0000

    IntValue

---
+++ Scripts/Data/IntValue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GameTemplate
+{
+    [CreateAssetMenu(menuName = "Data/IntValue")]
+    public class IntValue : GenericValue<int>
+    {
+        public void Add(int i)
+        {
+            Value += i;
+        }
+
+        public void Increment()
+        {
+            Value++;
+        }
+
+        public void Decrement()
+        {
+            Value--;
+        }
+    }
+}
---
+++ Scripts/Events/Listeners/IntValueListener.cs
@@ -0,0 +1,37 @@
+﻿namespace GameTemplate
+{
+    public class IntValueListener : GenericValueListener<IntValue, int>
+    {
+        public ValueListenerType Type = ValueListenerType.Any;
+        public int ReferenceValue;
+        public UnityEventInt Callback;
+
+        protected override void Listener(int arg)
+        {
+            switch (Type)
+            {
+                case ValueListenerType.LessThan:
+                    if (arg < ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.LessThanOrEquals:
+                    if (arg <= ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.GreaterThan:
+                    if (arg > ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.GreaterThanOrEquals:
+                    if (arg >= ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.Equals:
+                    if (arg == ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.Not:
+                    if (arg != ReferenceValue) Callback.Invoke(arg);
+                    break;
+                case ValueListenerType.Any:
+                    Callback.Invoke(arg);
+                    break;
+            }
+        }
+    }
+}
