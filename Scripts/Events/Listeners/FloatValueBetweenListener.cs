4fea9166c4546f9f3d4a36ce102ee42ce9fe1944
Daniel <daniel@shed-works.co.uk>
2018-02-16 16:58:22 +0000

    FloatValueBetweenListener

---
+++ Scripts/Events/Listeners/FloatValueBetweenListener.cs
@@ -0,0 +1,16 @@
+﻿using GameTemplate;
+using UnityEngine;
+
+public class FloatValueBetweenListener : GenericValueListener<FloatValue, float>
+{
+    public float Min;
+    public float Max;
+    public GameObject SetActiveObject;
+    
+    protected override void Listener(float arg)
+    {
+        var active = arg >= Min && arg <= Max;
+        
+        if(SetActiveObject.activeSelf != active) SetActiveObject.SetActive(active);
+    }
+}
