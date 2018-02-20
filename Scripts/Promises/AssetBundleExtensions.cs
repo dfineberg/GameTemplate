e1366d9013a708ab93bba0cdb7b5ab698156d83a
Daniel <daniel@shed-works.co.uk>
2018-02-20 18:11:58 +0000

    Refactored the AssetBundleExtension API to cache loaded bundles and work in the editor without building all the bundles manually

--- Scripts/Promises/AssetBundleExtensions.cs
+++ Scripts/Promises/AssetBundleExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 #if UNITY_EDITOR
@@ -9,23 +10,32 @@ namespace GameTemplate.Promises
 {
     public static class AssetBundleExtensions
     {
-        public static IPromise LoadFromFileAsync(string streamingAssetsPath)
+        private static readonly Dictionary<string, AssetBundle> LoadedBundles = new Dictionary<string, AssetBundle>();
+        
+        private static IPromise LoadFromFileAsync(string bundlePath)
         {
+            if (LoadedBundles.ContainsKey(bundlePath))
+                return Promise.Resolved(LoadedBundles[bundlePath]);
+            
             var p = new Promise();
-            var path = Path.Combine(Application.streamingAssetsPath, streamingAssetsPath).Replace("\\", "/");
+            var path = Path.Combine(Application.streamingAssetsPath, bundlePath).Replace("\\", "/");
             var request = AssetBundle.LoadFromFileAsync(path);
             
             CoroutineExtensions.WaitUntil(request)
-                .ThenLog("Loaded AssetBundle at: " + path)
-                .ThenDo(() => p.Resolve(request.assetBundle));
+                .ThenDo(() =>
+                {
+                    Debug.Log($"Loaded AssetBundle at: {path}");
+                    LoadedBundles.Add(bundlePath, request.assetBundle);
+                    p.Resolve(request.assetBundle);
+                });
             
             return p;
         }
 
-        public static IPromise LoadFromFileAndUnpack(string streamingAssetsPath)
+        public static IPromise LoadAllAssets(string bundlePath)
         {
 #if UNITY_EDITOR
-            var paths = AssetDatabase.GetAssetPathsFromAssetBundle(streamingAssetsPath);
+            var paths = AssetDatabase.GetAssetPathsFromAssetBundle(bundlePath);
             var assets = paths.Select(AssetDatabase.LoadAssetAtPath<Object>).ToArray();
             return Promise.Resolved(assets);
 #else
@@ -34,15 +44,48 @@ namespace GameTemplate.Promises
 #endif
         }
 
-        public static IPromise LoadAllAssetsPromise(this AssetBundle bundle)
+        public static IPromise LoadAllAssetNames(string bundlePath)
         {
-            return LoadAllAssetsPromise<object>(bundle);
+#if UNITY_EDITOR
+            return Promise.Resolved(AssetDatabase.GetAssetPathsFromAssetBundle(bundlePath));
+#else
+            return LoadFromFileAsync(streamingAssetsPath)
+                .Then<AssetBundle>(b => Promise.Resolved(b.GetAllAssetNames()));
+#endif
         }
 
-        public static IPromise LoadAllAssetsPromise<T>(this AssetBundle bundle)
+        public static IPromise LoadAsset(string bundlePath, string assetName)
+        {
+#if UNITY_EDITOR
+            var assetPath = AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(bundlePath, assetName)[0];
+            return Promise.Resolved(AssetDatabase.LoadAssetAtPath<Object>(assetPath));
+#else       
+            return LoadFromFileAsync(bundlePath)
+                .Then<AssetBundle>(b => b.LoadAssetPromise(assetName));
+#endif
+        }
+
+        public static void Unload(string bundlePath, bool unloadAllLoadedObjects)
+        {
+            if (!LoadedBundles.ContainsKey(bundlePath))
+                return;
+            
+            LoadedBundles[bundlePath].Unload(unloadAllLoadedObjects);
+        }
+
+        public static void UnloadAll(bool unloadAllObjects)
+        {
+            LoadedBundles.Clear();
+            AssetBundle.UnloadAllAssetBundles(unloadAllObjects);
+        }
+
+        
+        // --------- AssetBundle extension methods --------- //
+        
+        public static IPromise LoadAllAssetsPromise(this AssetBundle bundle)
         {
             var p = new Promise();
-            var request = bundle.LoadAllAssetsAsync(typeof(T));
+            var request = bundle.LoadAllAssetsAsync();
 
             CoroutineExtensions.WaitUntil(request)
                 .ThenDo(() => p.Resolve(request.allAssets));
