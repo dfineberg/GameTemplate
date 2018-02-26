86c7dda0ce0079e998dfe42b28ea503f7eee709d
Daniel <daniel@shed-works.co.uk>
2018-02-26 19:08:54 +0000

    Static ObjectPool for non-monobehaviour pools

---
+++ Scripts/Pooling/ObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace GameTemplate
+{
+    public static class ObjectPool
+    {
+        private static class GenericObjectPool<T> where T : new()
+        {
+            private static readonly Stack<T> Pool = new Stack<T>();
+
+            public static T Pop()
+            {
+                return Pool.Count == 0 ? new T() : Pool.Pop();
+            }
+
+            public static void Push(T obj)
+            {
+                if (Pool.Contains(obj)) return;
+                Pool.Push(obj);
+            }
+        }
+
+        public static void Push<T>(T obj) where T : new()
+        {
+            GenericObjectPool<T>.Push(obj);
+        }
+
+        public static T Pop<T>() where T : new()
+        {
+            return GenericObjectPool<T>.Pop();
+        }
+    }
+}
