b7dce0ee0231a470afc84d011ff91c8c3e7cb4c3
Daniel <daniel@shed-works.co.uk>
2018-05-11 00:41:16 +0100

    ValueSetter

---
+++ Scripts/Data/ValueSetter.cs
@@ -0,0 +1,14 @@
+﻿using GameTemplate;
+using UnityEngine;
+
+public class ValueSetter : MonoBehaviour
+{
+    public BoolValue BoolValue;
+    public IntValue IntValue;
+    public FloatValue FloatValue;
+
+    public void SetBool(bool b) => BoolValue.Value = b;
+    public void SetBoolWithInt(int i) => BoolValue.Value = i != 0; // animation events can't pass bool values
+    public void SetInt(int i) => IntValue.Value = i;
+    public void SetFloat(float f) => FloatValue.Value = f;
+}
