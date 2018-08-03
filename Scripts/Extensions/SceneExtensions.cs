b0a606ef467b671737758a5d715d93f5f22169b9
Daniel <daniel@shed-works.co.uk>
2018-08-03 16:35:22 +0100

    SceneExtensions

---
+++ Scripts/Extensions/SceneExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public static class SceneExtensions
+{
+    public static T FindObjectOfType<T>(this Scene scene)
+    {
+        foreach (var gameObject in scene.GetRootGameObjects())
+        {
+            var t = gameObject.GetComponentInChildren<T>(true);
+            if (t != null) return t;
+        }
+
+        return default(T);
+    }
+
+    public static T[] FindObjectsOfType<T>(this Scene scene)
+    {
+        var list = new List<T>();
+
+        foreach (var gameObject in scene.GetRootGameObjects())
+            list.AddRange(gameObject.GetComponentsInChildren<T>(true));
+
+        return list.ToArray();
+    }
+}
