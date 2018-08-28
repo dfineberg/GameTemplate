4af1074441b34cc121af90e7d08259089b5cd55a
Daniel <daniel@shed-works.co.uk>
2018-08-28 18:12:16 +0100

    StaticUpdate

---
+++ Scripts/Events/StaticUpdate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace GameTemplate
+{
+    public class StaticUpdate : MonoBehaviour
+    {
+        public delegate void UpdateDelegate();
+            
+        private static StaticUpdate _instance;
+
+        private event UpdateDelegate Callback;
+        
+        private static void Init()
+        {
+            var obj = new GameObject("StaticUpdate") {hideFlags = HideFlags.HideInHierarchy};
+            DontDestroyOnLoad(obj);
+            _instance = obj.AddComponent<StaticUpdate>();
+        }
+
+        public static void Subscribe(UpdateDelegate callback)
+        {
+            if (!Application.isPlaying)
+                return;
+            
+            if (!_instance)
+                Init();
+
+            _instance.Callback += callback;
+        }
+
+        public static void Unsubscribe(UpdateDelegate callback)
+        {
+            if (!Application.isPlaying)
+                return;
+            
+            if (!_instance)
+                Init();
+
+            _instance.Callback -= callback;
+        }
+
+        private void Update()
+        {
+            Callback?.Invoke();
+        }
+    }
+}
