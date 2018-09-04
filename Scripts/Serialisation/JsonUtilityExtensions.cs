ad3cec8458587a4e54f2a3c13ff3561c4dea23d9
Daniel <daniel@shed-works.co.uk>
2018-09-04 16:38:03 +0100

    JsonUtilityExtensions

---
+++ Scripts/Serialisation/JsonUtilityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public static class JsonUtilityExtensions
+{
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] Array;
+    }
+    
+    public static T[] FromJsonArray<T>(string json)
+    {
+        var newJson = "{ \"Array\": " + json + "}";
+        var wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.Array;
+    }
+}
