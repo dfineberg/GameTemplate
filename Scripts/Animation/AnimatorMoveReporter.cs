3a4d0a687faebcda767517d4a894e6508c0fc801
Daniel <daniel@shed-works.co.uk>
2018-10-16 17:46:56 +0100

    AnimatorMoveReporter

---
+++ Scripts/Animation/AnimatorMoveReporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimatorMoveReporter : MonoBehaviour
+{
+    public event Action OnAnimatorMoveEvent;
+
+    private void OnAnimatorMove()
+    {
+        OnAnimatorMoveEvent?.Invoke();
+    }
+}
