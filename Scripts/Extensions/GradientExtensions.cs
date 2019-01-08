6f21b6b958bd8f7e47cc7a5d4668423d0a24366b
Daniel <daniel@shed-works.co.uk>
2019-01-08 14:07:49 +0000

    GradientExtensions

---
+++ Scripts/Extensions/GradientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class GradientExtensions
+{
+    public static Texture2D GetTexture(this Gradient gradient, int resolution)
+    {
+        var tex = new Texture2D(resolution, 1, TextureFormat.ARGB32, false)
+        {
+            wrapMode = TextureWrapMode.Clamp
+        };
+
+        var colours = new Color[resolution];
+
+        for (var i = 0; i < resolution; i++) 
+            colours[i] = gradient.Evaluate((float) i / resolution);
+        
+        tex.SetPixels(colours);
+        tex.Apply();
+
+        return tex;
+    }
+}
