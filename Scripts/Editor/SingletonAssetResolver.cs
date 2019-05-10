a6ad790c5ccbb7cd6e25b0e9eedfc78f8287f05b
Daniel Fineberg <daniel@shed-works.co.uk>
2019-05-10 13:45:03 +0100

    New SingletonAsset system

---
+++ Scripts/Editor/SingletonAssetResolver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class SingletonAssetResolver
+{
+    static SingletonAssetResolver()
+    {
+        Resolve();
+    }
+
+    private static void Resolve()
+    {
+        if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+        {
+            AssetDatabase.CreateFolder("Assets", "Resources");
+            AssetDatabase.Refresh();
+        }
+
+        var singletonTypes = SingletonAsset.GetTypes();
+        var assetCreated = false;
+
+        foreach (var type in singletonTypes)
+        {
+            var assetPath = $"Assets/Resources/{type.Name}.asset";
+            if (AssetDatabase.LoadAssetAtPath(assetPath, type) != null) continue;
+
+            Debug.Log($"Creating new SingletonAsset: {type.Name}");
+            assetCreated = true;
+            var newAsset = ScriptableObject.CreateInstance(type);
+            AssetDatabase.CreateAsset(newAsset, assetPath);
+        }
+
+        if (assetCreated) AssetDatabase.Refresh();
+    }
+}
---
+++ Scripts/SingletonAsset/SingletonAsset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameTemplate.Promises;
+using UnityEngine;
+
+/// <summary>
+/// The SingletonAsset system is a way of creating singleton classes that are ScriptableObjects.
+/// To make a new singleton, just create a new class that inherits from SingletonAsset, and the SingletonAssetResolver will
+/// create a new instance of it in the Resources folder.
+/// </summary>
+public abstract class SingletonAsset : ScriptableObject
+{
+    private static readonly Dictionary<Type, SingletonAsset> AssetDictionary = new Dictionary<Type, SingletonAsset>();
+
+    public static bool Loaded { get; private set; }
+
+    public static IPromise LoadAll()
+    {
+        var types = GetTypes();
+        var loadPromises = types.Select(t =>
+        {
+            var argName = t.Name;
+            return ResourceExtensions.LoadAsync(argName);
+        });
+
+        return Promise.All(loadPromises)
+            .ThenDo<object[]>(assets =>
+            {
+                for (var i = 0; i < assets.Length; i++)
+                {
+                    var singletonAsset = (SingletonAsset) assets[i];
+                    AssetDictionary.Add(types[i], singletonAsset);
+                }
+            })
+            .ThenAll(() => AssetDictionary.Values.Select(asset => asset.OnAssetsLoaded()))
+            .ThenDo(() => Loaded = true);
+    }
+
+    public static Type[] GetTypes()
+    {
+        return AppDomain.CurrentDomain.GetAssemblies() // in all the currently loaded assemblies,
+            .SelectMany(s => s.GetTypes()) // get all the types
+            .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(SingletonAsset))) // that inherit from SingletonAsset
+            .ToArray();
+    }
+
+    public static T Instance<T>() where T : SingletonAsset
+    {
+        Debug.Assert(Loaded, "SingletonAssets haven't been loaded yet.");
+        Debug.Assert(AssetDictionary.ContainsKey(typeof(T)),
+            $"Something went wrong loading the SingletonAsset {nameof(T)}");
+        
+        return (T) AssetDictionary[typeof(T)];
+    }
+
+    /// <summary>
+    /// Called after all SingletonAssets have been loaded.
+    /// The order in which the assets are initialised cannot be guaranteed,
+    /// so make sure no asset relies on another asset being initialised.
+    /// </summary>
+    protected virtual IPromise OnAssetsLoaded()
+    {
+        return Promise.Resolved();
+    }
+}
