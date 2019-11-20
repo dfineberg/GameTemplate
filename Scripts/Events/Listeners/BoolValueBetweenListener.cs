de18983b278bd411554d1567b12117a28bf048d9
Daniel Fineberg <daniel@shed-works.co.uk>
2019-11-20 10:56:17 +0000

    BoolValueBetweenListener

---
+++ Scripts/Events/Listeners/BoolValueBetweenListener.cs
@@ -0,0 +1,29 @@
+﻿using GameTemplate;
+using UnityEngine;
+
+public class BoolValueBetweenListener : MonoBehaviour
+{
+    public BoolValue FirstValue;
+    public BoolValue SecondValue;
+    public bool FireOnEnable;
+    public UnityEventBool Callback;
+
+    private void OnEnable()
+    {
+        if (FireOnEnable) Evaluate(false);
+        FirstValue.Subscribe(Evaluate);
+        SecondValue.Subscribe(Evaluate);
+    }
+
+    private void OnDisable()
+    {
+        FirstValue.Unsubscribe(Evaluate);
+        SecondValue.Unsubscribe(Evaluate);
+    }
+
+    private void Evaluate(bool b)
+    {
+        var param = FirstValue.Value && !SecondValue.Value;
+        Callback.Invoke(param);
+    }
+}
