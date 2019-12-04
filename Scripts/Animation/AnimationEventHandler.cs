04296f46b9c31d0b3093569991e266e309b2c5f6
Daniel Fineberg <daniel@shed-works.co.uk>
2019-12-04 15:17:17 +0000

    Refactored AnimationEventHandler to have UnityEvent callbacks

--- Scripts/Animation/AnimationEventHandler.cs
+++ Scripts/Animation/AnimationEventHandler.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GameTemplate
 {
     public class AnimationEventHandler : MonoBehaviour
     {
+        [Serializable]
+        public struct AnimationUnityEvent
+        {
+            public string Name;
+            public UnityEvent Callback;
+        }
         public event Action<string> AnimationEvent;
+        public AnimationUnityEvent[] Callbacks;
 
         public void FireEvent(string id)
         {
             if (AnimationEvent != null)
                 AnimationEvent(id);
+
+            foreach (var callback in Callbacks)
+                if (callback.Name == id)
+                    callback.Callback.Invoke();
         }
     }
 }
