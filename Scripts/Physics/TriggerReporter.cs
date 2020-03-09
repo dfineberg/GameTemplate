1f007cc24244d3039fe30f5c0f58fae355886dab
Daniel <enteraddresshere@gmail.com>
2020-03-09 16:50:53 +0000

    Added a new set of events to make TriggerReporter more useful when created with code

--- Scripts/Physics/TriggerReporter.cs
+++ Scripts/Physics/TriggerReporter.cs
@@ -16,15 +16,19 @@ namespace GameTemplate
         public TriggerType Type = TriggerType.PhysicsLayers;
         public LayerMask TriggerLayers = Physics.AllLayers;
         public string TriggerTag;
-        public ColliderEvent OnTriggerEnterEvent;
-        public ColliderEvent OnTriggerExitEvent;
+        [SerializeField] private ColliderEvent OnTriggerEnterEvent;
+        [SerializeField] private ColliderEvent OnTriggerExitEvent;
+
+        public Action<Collider> TriggerEnterEvent;
+        public Action<Collider> TriggerExitEvent;
 
         private void OnTriggerEnter(Collider other)
         {
             if (!CheckObject(other.gameObject)) 
                 return;
         
-            OnTriggerEnterEvent.Invoke(other);
+            OnTriggerEnterEvent?.Invoke(other);
+            TriggerEnterEvent?.Invoke(other);
         }
 
         private void OnTriggerExit(Collider other)
@@ -32,7 +36,8 @@ namespace GameTemplate
             if (!CheckObject(other.gameObject)) 
                 return;
         
-            OnTriggerExitEvent.Invoke(other);
+            OnTriggerExitEvent?.Invoke(other);
+            TriggerExitEvent?.Invoke(other);
         }
 
         private bool CheckObject(GameObject obj)
