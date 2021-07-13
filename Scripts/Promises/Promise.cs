3b6b8a81a6580f7cf9e42bc6f824b205a2ae80de
Dario Zubovic <dario@rawfury.com>
2021-07-14 00:08:41 +0200

    Removed faulty promise resultion disposals

--- Scripts/Promises/Promise.cs
+++ Scripts/Promises/Promise.cs
@@ -80,10 +80,6 @@ namespace GameTemplate.Promises
         {
             if (CurrentState == EPromiseState.Pooled) return;
             
-            foreach (var resolution in _resolutions)
-            {
-                resolution.Dispose();
-            }
             _resolutions.Clear();
             _rejectCallbacks.Clear();
             RejectedException = null;
