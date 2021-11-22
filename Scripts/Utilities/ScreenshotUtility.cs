2f5af95221ebce30ac87eae192b736496dee72e1
Dario <dario@rawfury.com>
2021-11-22 01:05:21 +0100

    Make screenshots method work on platforms without async GPU readback too

--- Scripts/Utilities/ScreenshotUtility.cs
+++ Scripts/Utilities/ScreenshotUtility.cs
@@ -20,38 +20,47 @@ namespace GameTemplate
             CoroutineExtensions.WaitForEndOfFrame()
                 .ThenDo(() =>
                 {
-                    if (screenSize != previousScreenSize)
+                    if (SystemInfo.supportsAsyncGPUReadback)
                     {
-                        if (screenshotTexture != null)
+                        if (screenSize != previousScreenSize)
                         {
-                            Destroy();
+                            if (screenshotTexture != null)
+                            {
+                                Destroy();
+                            }
+
+                            screenshotTexture = new RenderTexture(screenSize.x, screenSize.y, 0, RenderTextureFormat.ARGB32, 0);
+                            screenshotTexture.name = "ScreenshotWriteTex";
+
+                            buffer = new NativeArray<byte>(screenSize.x * screenSize.y * 3, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
                         }
+                        
+                        var flipped = RenderTexture.GetTemporary(screenshotTexture.descriptor);
+                        ScreenCapture.CaptureScreenshotIntoRenderTexture(flipped);
+                        Graphics.Blit(flipped, screenshotTexture, new Vector2(1, -1), new Vector2(0, 1));
+                        RenderTexture.ReleaseTemporary(flipped);
+                        
+                        AsyncGPUReadback.RequestIntoNativeArray(ref buffer, screenshotTexture, 0, TextureFormat.RGB24, (AsyncGPUReadbackRequest request) =>
+                        {
+                            if (request.hasError)
+                            {
+                                Debug.LogError("AsyncGPUReadback encountered an error");
+                                dataWriteCallback(null);
+                                return;
+                            }
 
-                        screenshotTexture = new RenderTexture(screenSize.x, screenSize.y, 0, RenderTextureFormat.ARGB32, 0);
-                        screenshotTexture.name = "ScreenshotWriteTex";
-
-                        buffer = new NativeArray<byte>(screenSize.x * screenSize.y * 3, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                            using (NativeArray<byte> encodedBytes = ImageConversion.EncodeNativeArrayToPNG(buffer, GraphicsFormat.R8G8B8_SRGB, (uint)screenSize.x, (uint)screenSize.y))
+                            {
+                                dataWriteCallback(encodedBytes.ToArray());
+                            }
+                        }); 
                     }
-                    
-                    var flipped = RenderTexture.GetTemporary(screenshotTexture.descriptor);
-                    ScreenCapture.CaptureScreenshotIntoRenderTexture(flipped);
-                    Graphics.Blit(flipped, screenshotTexture, new Vector2(1, -1), new Vector2(0, 1));
-                    RenderTexture.ReleaseTemporary(flipped);
-                    
-                    AsyncGPUReadback.RequestIntoNativeArray(ref buffer, screenshotTexture, 0, TextureFormat.RGB24, (AsyncGPUReadbackRequest request) =>
+                    else
                     {
-                        if (request.hasError)
-                        {
-                            Debug.LogError("AsyncGPUReadback encountered an error");
-                            dataWriteCallback(null);
-                            return;
-                        }
-
-                        using (NativeArray<byte> encodedBytes = ImageConversion.EncodeNativeArrayToPNG(buffer, GraphicsFormat.R8G8B8_SRGB, (uint)screenSize.x, (uint)screenSize.y))
-                        {
-                            dataWriteCallback(encodedBytes.ToArray());
-                        }
-                    }); 
+                        var screenshotTex = ScreenCapture.CaptureScreenshotAsTexture();
+                        var bytes = ImageConversion.EncodeToPNG(screenshotTex);
+                        dataWriteCallback(bytes);
+                    }
                 });
         }
 
