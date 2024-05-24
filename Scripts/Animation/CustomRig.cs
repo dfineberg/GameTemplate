c2d6d2fc3a38bfb5cbf7566f3f940b0155170411
Daniel Fineberg <daniel@shed-works.co.uk>
2024-05-24 15:03:26 +0100

    Talking system in CustomRig

--- Scripts/Animation/CustomRig.cs
+++ Scripts/Animation/CustomRig.cs
@@ -1,10 +1,15 @@
 using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class CustomRig : MonoBehaviour
 {
+    #region Animation Handles
+
     [Serializable]
     public struct AxisInfo
     {
@@ -82,7 +87,77 @@ public class CustomRig : MonoBehaviour
         public bool DrawHandle = true;
     }
 
+    #endregion
+
     [FormerlySerializedAs("PositionHandles")] [FormerlySerializedAs("Handles")] public HandleInfo[] BlendShapeHandles;
     [FormerlySerializedAs("RotationHandles")] public RotationHandle[] BoneHandles;
     [InlineEditor] public SkinnedMeshRenderer Renderer;
+
+    public AnimationClip[] ConsonantClips;
+    public AnimationClip[] VowelClips;
+    public bool Talk;
+    public float MouthShapeTime = 0.1f;
+    
+    private PlayableGraph _talkingGraph;
+    private AnimationMixerPlayable _talkingMixerPlayable;
+
+    private int MouthClipCount => ConsonantClips.Length + VowelClips.Length;
+
+    private void Start()
+    {
+        InitTalkingGraph();
+    }
+
+    private void Update()
+    {
+        UpdateTalkingGraph();
+    }
+
+    private void InitTalkingGraph()
+    {
+        if (ConsonantClips.Length == 0 || VowelClips.Length == 0)
+            return;
+
+        var animator = GetComponent<Animator>();
+        _talkingMixerPlayable = AnimationPlayableUtilities.PlayMixer(animator, MouthClipCount, out _talkingGraph);
+
+        for (var i = 0; i < ConsonantClips.Length; i++)
+        {
+            var clipPlayable = AnimationClipPlayable.Create(_talkingGraph, ConsonantClips[i]);
+            _talkingGraph.Connect(clipPlayable, 0, _talkingMixerPlayable, i);
+        }
+
+        for (var i = 0; i < VowelClips.Length; i++)
+        {
+            var clipPlayable = AnimationClipPlayable.Create(_talkingGraph, VowelClips[i]);
+            _talkingGraph.Connect(clipPlayable, 0, _talkingMixerPlayable, ConsonantClips.Length + i);
+        }
+    }
+
+
+    private void UpdateTalkingGraph()
+    {
+        if (!_talkingGraph.IsValid())
+            return;
+        
+        if (!Talk)
+        {
+            if (_talkingGraph.IsPlaying()) 
+                _talkingGraph.Stop();
+            
+            return;
+        }
+
+        if (!_talkingGraph.IsPlaying()) 
+            _talkingGraph.Play();
+
+        var shapeIndex = (int) (Time.time / MouthShapeTime);
+        var isVowel = shapeIndex % 2 == 1;
+        Random.InitState(shapeIndex);
+        shapeIndex = Random.Range(0, isVowel ? VowelClips.Length : ConsonantClips.Length);
+        if (isVowel) shapeIndex += ConsonantClips.Length;
+
+        for (var i = 0; i < MouthClipCount; i++)
+            _talkingMixerPlayable.SetInputWeight(i, i == shapeIndex ? 1f : 0f);
+    }
 }
